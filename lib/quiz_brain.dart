385cae88aa0b3c058a74c8f8d9b9d2e43b138084
66rhythm <tanyabh2k1@gmail.com>
2021-02-06 04:02:29 +0530

    initial commit

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,57 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+  List<Question> _questionBank = [
+    Question(
+        q: 'The first version of Flutter was known as codename Sky and ran on the Android operating system',
+        a: true),
+    Question(q: 'A container can have multiple children', a: true),
+    Question(q: 'SetState function is used with statless widgets', a: false),
+    Question(
+        q: 'No piece of square dry paper can be folded in half more than 7 times.',
+        a: false),
+    Question(
+        q: 'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        a: true),
+    Question(
+        q: 'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        a: false),
+    Question(
+        q: 'The total surface area of two human lungs is approximately 70 square metres.',
+        a: true),
+    Question(
+        q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        a: true),
+    Question(
+        q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        a: true),
+  ];
+
+  String getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('Now returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void reset() {
+    _questionNumber = 0;
+  }
+}
